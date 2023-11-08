aaf3bc02b09215779022b2c2986f726869cea205
DucAnhNTT <ducanh0285@gmail.com>
2023-11-08 10:50:16 +0700

    Initial commit

--- CalculatorTester/UnitTest1.cs
+++ CalculatorTester/UnitTest1.cs
@@ -24,7 +24,7 @@ namespace CalculatorTester
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
+        [TestMethod] //TC2: a=10, b=5, kq=5
         public void TestTru()
         {
             int expected, actual;
@@ -35,7 +35,7 @@ namespace CalculatorTester
         }
 
         [TestMethod]
-        public void TestNhan()
+        public void TestNhan() //TC3: a=10, b=5, kq=50
         {
             int expected, actual;
             //Calculation c = new Calculation(a,b);
@@ -44,7 +44,7 @@ namespace CalculatorTester
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
+        [TestMethod] //TC4: a=10, b=5, kq=2 
         public void TestChia()
         {
             int expected, actual;
@@ -55,12 +55,42 @@ namespace CalculatorTester
         }
 
 
-        [TestMethod]
+        [TestMethod] //TC5: a=10, b=0, kq=DivideByZeroException
         [ExpectedException(typeof(DivideByZeroException))]
         public void Test_ChiaZero()
         {
             c = new Calculation(10, 0);
             c.Execute("/");
         }
+
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\TestData.csv", "TestData#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestWithDataSource()
+        {
+            int a = int.Parse(TestContext.DataRow[0].ToString());
+            int b = int.Parse(TestContext.DataRow[1].ToString());
+            int expected = int.Parse(TestContext.DataRow[2].ToString());
+
+            Calculation c = new Calculation(a, b);
+            int actual = c.Execute("+");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", 
+            @".\Data\DataTest_cotToantu.csv", "DataTest_cotToantu#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestWithDataSourceAndOperation()
+        {
+            int a = int.Parse(TestContext.DataRow[0].ToString());
+            int b = int.Parse(TestContext.DataRow[1].ToString());
+            string operation = TestContext.DataRow[2].ToString();          
+            int expected = int.Parse(TestContext.DataRow[3].ToString());
+
+            Calculation c = new Calculation(a, b);
+            int actual = c.Execute(operation.Replace("'",""));
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
